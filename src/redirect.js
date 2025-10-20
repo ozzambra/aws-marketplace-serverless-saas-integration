ee275a988ad4c3259a0a580a30941528eccf9d6d
Workshop Participant <participant@workshops.aws>
2025-10-20 07:53:07 +0000

    delete sns paramter from template

--- src/redirect.js
+++ src/redirect.js
@@ -1,12 +1,9 @@
 const redirectToProductPage = process.env.VARIABLE_NAME || 'false';
 
-
 exports.redirecthandler = async(event, context, callback) => {
-  
+  console.log("event:", event);
   const redirectUrl = "/?" + event['body'];
-  
-
-
+  console.log("redirectUrl:", redirectUrl);
 
   const response = {
       statusCode: 302,
