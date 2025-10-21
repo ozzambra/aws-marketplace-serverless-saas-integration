186856802dc0841a49bd8c94c85786e5ca78c9ac
Workshop Participant <participant@workshops.aws>
2025-10-21 17:08:43 +0000

    template: log groups for event bridge, updates to subscription handler

--- src/subscription-sqs.js
+++ src/subscription-sqs.js
@@ -4,6 +4,7 @@ const { SupportSNSArn: TopicArn, NewSubscribersTableName: newSubscribersTableNam
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10', region: aws_region });
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 const { MarketplaceEntitlementServiceClient, GetEntitlementsCommand } = require("@aws-sdk/client-marketplace-entitlement-service");
+const { MarketplaceCatalogClient, DescribeEntityCommand } = require('@aws-sdk/client-marketplace-catalog');
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: winston.format.json(),
@@ -30,27 +31,43 @@ async function getEntitlements(productCode, customerAccountId, region) {
   }
 }
 
-
 exports.SQSHandler = async (event) => {
-  logger.info('event:', JSON.stringify(event, null, 2));
+  logger.info('logger event:', event);
+  console.log('console event:', event);
   await Promise.all(event.Records.map(async (record) => {
     const { body } = record;
-    let { Message: message } = JSON.parse(body);
+    console.log('body:', body);
+    console.log('typeof body:', typeof body);
 
-    if (typeof message === 'string' || message instanceof String) {
-      message = JSON.parse(message);
-    }
-    logger.info(`message: ${JSON.stringify(message, null, 2)}`);
+    const message = typeof body === 'string' ? JSON.parse(body) : body;
+
+    //let { Message: message } = JSON.parse(body);
+    console.log('message:', message);
+
+    //if (typeof message === 'string' || message instanceof String) {
+    //  message = JSON.parse(message);
+    //}
+    //console.log('message:', message);
+    logger.info('This is an info message');
+
+    //logger.info(`message: ${JSON.stringify(message, null, 2)}`);
+    
 
     let successfullySubscribed = false;
     let subscriptionExpired = false;
 
     // subscribe-success - License Updated (entitlement-sqs.js)
     // update entitlement-sqs.js to make DDB entry
-    if (message.action === 'subscribe-success') {
+    console.log('message.detail-type:', message['detail-type']);
+    if (message['detail-type'] === 'Purchase Agreement Created - Proposer') {
+    //if (message.action === 'subscribe-success') {
+      console.log('DETAILPURCHASE: Purchase Agreement Created - Proposer');
       successfullySubscribed = true;
     //  Purchase Agreement Ended / Status TERMINATED
-    } else if (message.action === 'unsubscribe-pending') {
+    // 'Purchase Agreement Ended - Proposer'
+    //} else if (message.action === 'unsubscribe-pending') {
+    } else if (message['detail-type'] === 'Purchase Agreement Ended - Proposer') {
+      console.log('DETAILPURCHASE: Purchase Agreement Ended - Proposer');
       logger.info(`unsubscribe-pending: sending message to topic ${TopicArn}`);
       const SNSparams = {
         TopicArn,
@@ -80,14 +97,55 @@ exports.SQSHandler = async (event) => {
      isFreeTrialTermPresent = message.isFreeTrialTermPresent.toLowerCase() === "true";
     }
 
-    let dynamoDbKey = message['customer-identifier']
+    let acceptorAccountId;
+    if (message['detail']['acceptor']['accountId']) {
+      acceptorAccountId = message['detail']['acceptor']['accountId'];
+      console.log('acceptorAccountId:', acceptorAccountId);
+    }
+    let offerId;
+    let productId;
+    let productCode;
+    if (message['detail']['offer']['id']) {
+      offerId = message['detail']['offer']['id'];
+      console.log('offerId:', offerId);
+      const mpCatClient = new MarketplaceCatalogClient();
 
-    if (!message['customer-aws-account-id']) {
+      const response = await mpCatClient.send(new DescribeEntityCommand({
+        Catalog: 'AWSMarketplace',
+        EntityId: offerId
+      }));
+      console.log('responseofferId:', response);
+      productId = response['DetailsDocument']['ProductId'];
+      console.log('productId:', productId);
+
+      let response2;
+      try {
+        response2 = await mpCatClient.send(new DescribeEntityCommand({
+          Catalog: 'AWSMarketplace',
+          EntityId: productId
+        }));
+      } catch (error) {
+        console.error('Error getting product details:', error);
+        return;
+      }
+      console.log('responseproductId:', response2);
+      productCode = response2['DetailsDocument']['Description']['ProductCode'];
+      console.log('productCode:', productCode);
+      //const productCode = details.ProductCode;
+
+    }
+
+    //let dynamoDbKey = message['customer-identifier']
+    let dynamoDbKey = acceptorAccountId
+
+    //if (!message['customer-aws-account-id']) {
+    if (!acceptorAccountId) {
       logger.error('customer-aws-account-id not found in message. Trying GetEntitlements');
 
       const entitlementsResponse = await getEntitlements(
-        message['product-code'], 
-        message['customer-aws-account-id'],
+        //message['product-code'],
+        productCode,
+        acceptorAccountId,
         aws_region
       );
       
@@ -108,7 +166,7 @@ exports.SQSHandler = async (event) => {
       },
       UpdateExpression: 'set subscription_action = :ac, successfully_subscribed = :ss, subscription_expired = :se, is_free_trial_term_present = :ft',
       ExpressionAttributeValues: {
-        ':ac': { S: message['action'] },
+        ':ac': { S: message['detail-type'] },
         ':ss': { BOOL: successfullySubscribed },
         ':se': { BOOL: subscriptionExpired },
         ':ft': { BOOL: isFreeTrialTermPresent}
