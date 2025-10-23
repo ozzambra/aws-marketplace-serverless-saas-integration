58842cebaea555caee8c13bed5462269afa988ac
Workshop Participant <participant@workshops.aws>
2025-10-23 08:50:27 +0000

    purchase agreements

--- src/subscription-sqs.js
+++ src/subscription-sqs.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 const { SupportSNSArn: TopicArn, NewSubscribersTableName: newSubscribersTableName, AWS_REGION: aws_region } = process.env;
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10', region: aws_region });
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
-const { MarketplaceEntitlementServiceClient, GetEntitlementsCommand } = require("@aws-sdk/client-marketplace-entitlement-service");
+//const { MarketplaceEntitlementServiceClient, GetEntitlementsCommand } = require("@aws-sdk/client-marketplace-entitlement-service");
 const { MarketplaceCatalogClient, DescribeEntityCommand } = require('@aws-sdk/client-marketplace-catalog');
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
@@ -13,22 +13,14 @@ const logger = winston.createLogger({
   ],
 });
 
-async function getEntitlements(productCode, customerAccountId, region) {
-  try {
-    logger.info(`getEntitlements: productCode: ${productCode}, customerAccountId: ${customerAccountId}, region: ${region}`);
-    const entitlementParams = {
-      ProductCode: productCode,
-      Filter: [customerAccountId] 
-    };
-    logger.debug('entitlementParams:', JSON.stringify(entitlementParams, null, 2));
-
-    const mpClient = new MarketplaceEntitlementServiceClient({ region });
-    const command = new GetEntitlementsCommand(entitlementParams);
-    return await mpClient.send(command);
-  } catch (error) {
-    logger.error('Error getting entitlements:', error);
-    return null;
-  }
+// publish message to SNS topic
+async function publishSNS(subject, message) {
+  const SNSparams = {
+    TopicArn,
+    Subject: subject,
+    Message: message,
+  };
+  await SNS.publish(SNSparams).promise();
 }
 
 exports.SQSHandler = async (event) => {
@@ -42,88 +34,84 @@ exports.SQSHandler = async (event) => {
 
     const message = typeof body === 'string' ? JSON.parse(body) : body;
 
-    //let { Message: message } = JSON.parse(body);
-    //console.log('message:', message);
-
-    //if (typeof message === 'string' || message instanceof String) {
-    //  message = JSON.parse(message);
-    //}
-    //console.log('message:', message);
-
     logger.info(`message: ${JSON.stringify(message, message, 2)}`);
     let successfullySubscribed = false;
     let subscriptionExpired = false;
 
     // subscribe-success - License Updated (entitlement-sqs.js)
-    // update entitlement-sqs.js to make DDB entry
-    //console.log('message.detail-type:', message['detail-type']);
+
+    // handle detail-type
     logger.info(`message.detail-type: ${message['detail-type']}`);
 
-    if (message['detail-type']?.startsWith('Purchase Agreement Ended')) {
+    //agreement data
+    let agreementId = null;
+    let agreementStatus = null;
+    try {
+      agreementId = message.detail.agreement.id;
+      agreementStatus = message.detail.agreement.status;
+      logger.info(`agreementId: ${agreementId} agreementStatus: ${agreementStatus}`);
+    } catch (e) {
+      logger.error('Error getting agreementId or agreementStatus:', e);
+      return;
+    }
+
+    if (!agreementStatus) {
+      logger.error('could not find agreementStatus');
+      return;
+    }
+
+    if (message['detail-type']?.startsWith('Purchase Agreement Created')) {
+      // Agreement Created
+      successfullySubscribed = true;
+      const isoString = new Date().toISOString();
+      await publishSNS(
+        `AWS Marketplace Agreement created: "${agreementId}" status "${agreementStatus}"`,
+        `Agreement created: ${JSON.stringify(message)}`
+      );
+
+    } else if (message['detail-type']?.startsWith('Purchase Agreement Amended')) {
+      // Agreement Amended
+      const isoString = new Date().toISOString();
+      await publishSNS(
+        `AWS Marketplace Agreement amended: "${agreementId}" status "${agreementStatus}"`,
+        `Agreement amended: ${JSON.stringify(message)}`
+      );
+    } else if (message['detail-type']?.startsWith('Purchase Agreement Ended')) {
+      // Agreement Ended
+      // ISV's (CPPO) want to recieve notification
+      // when purchase agreements is upgraded
       // get status CANCELLED | EXPIRED | RENEWED | REPLACED | TERMINATED
-      let agreementId = null;
-      let agreementStatus = null;
-      try {
-        agreementId = message.detail.agreement.id;
-        agreementStatus = message.detail.agreement.status;
-        logger.info(`agreementId: ${agreementId} agreementStatus: ${agreementStatus}`);
-      } catch (e) {
-        logger.error('Error getting agreementId and agreementStatus:', e);
-        return;
-      }
 
-      if (!agreementStatus) {
-        logger.error('could not find agreementStatus');
-        return;
-      }
-
-      // TERMINATED == subscribe-fail
       if (agreementStatus === 'TERMINATED') {
+        //  Purchase Agreement Ended / Status TERMINATED (subscribe-fail)
+        subscriptionExpired = true;
         logger.info(`agreementId "${agreementId}" ${agreementStatus} (subscribe-fail): sending message to topic ${TopicArn}`);
-        const SNSparams = {
-          TopicArn,
-          Subject: `AWS Marketplace Agreement "${agreementId}" status "${agreementStatus}"`,
-          Message: `Subscription failed: ${JSON.stringify(message)}`,
-        };
-        await SNS.publish(SNSparams).promise();
+        const isoString = new Date().toISOString();
+        await publishSNS(
+          `AWS Marketplace Agreement "${agreementId}" status "${agreementStatus}"`,
+          `Agreement terminated ${isoString}: ${JSON.stringify(message)}`
+        );
       } else if (['CANCELLED', 'EXPIRED'].includes(agreementStatus)) {
         // Cancelled, Expired) // metering records can still be send for 1 hour after receiving this event. Sending this events for Replaced, Renewed cases will be net new
         logger.info(`agreementId "${agreementId}" ${agreementStatus}: sending message to topic ${TopicArn}`);
         const isoString = new Date().toISOString();
-        const SNSparams = {
-          TopicArn,
-          Subject: `AWS Marketplace Agreement "${agreementId}" status "${agreementStatus}"`,
-          Message: `Subscription ended. You have 1h from ${isoString} to send metering records: ${JSON.stringify(message)}`,
-        };
-        await SNS.publish(SNSparams).promise();
+        await publishSNS(
+          `AWS Marketplace Agreement "${agreementId}" status "${agreementStatus}" - send metering records`,
+          `Agreement ended. You have 1h from ${isoString} to send metering records: ${JSON.stringify(message)}`
+        );
       } else {
-        logger.info(`status "${agreementStatus}" currently not handled`);
+        logger.info(`status "${agreementStatus}", sendinng generic message`);
+        logger.info(`agreementId "${agreementId}" ${agreementStatus}: sending message to topic ${TopicArn}`);
+        const isoString = new Date().toISOString();
+        await publishSNS(
+          `AWS Marketplace Agreement "${agreementId}" status "${agreementStatus}"`,
+          `Agreement ended: ${JSON.stringify(message)}`
+        );
       }
-
-    } else if (message['detail-type']?.startsWith('Purchase Agreement Created')) {
-      // subscribe-success is equal to License updated
-      // must go into entitlement
-      //if (message.action === 'subscribe-success') {
-      //console.log(`DETAILPURCHASE: {message['detail-type']}`);
-      successfullySubscribed = true;
-      //  Purchase Agreement Ended / Status TERMINATED
-      // 'Purchase Agreement Ended - Proposer'
-      //} else if (message.action === 'unsubscribe-pending') {
-    } else if (message.action === 'subscribe-fail') {
-      // Purchase Agreement Ended - Status TERMINATED
-      logger.info(`subscribe-fail: sending message to topic ${TopicArn}`);
-      const SNSparams = {
-        TopicArn,
-        Subject: 'AWS Marketplace Subscription failed',
-        Message: `Subscription failed: ${JSON.stringify(message)}`,
-      };
-
-      await SNS.publish(SNSparams).promise();
-    } else if (message.action === 'unsubscribe-success') {
-      subscriptionExpired = true;
+    // ISV's (MPPO) want to recieve notification when purchase agreements is upgraded
     } else {
-      logger.error('Unhandled action');
-      throw new Error(`Unhandled action - msg: ${JSON.stringify(record)}`);
+      logger.warn(`detail-type "${message['detail-type']}" not handled`);
+      //throw new Error(`Unhandled action - msg: ${JSON.stringify(record)}`);
     }
 
     let isFreeTrialTermPresent = false;
@@ -131,11 +119,16 @@ exports.SQSHandler = async (event) => {
      isFreeTrialTermPresent = message.isFreeTrialTermPresent.toLowerCase() === "true";
     }
 
+    // get parameters for DynamodDB
+    // we need the acceptor account id and product code
+    // acceptor account id
     let acceptorAccountId;
     if (message['detail']['acceptor']['accountId']) {
       acceptorAccountId = message['detail']['acceptor']['accountId'];
       logger.info(`acceptorAccountId: ${acceptorAccountId}`);
     }
+
+    // get offer from message
     let offerId;
     let productId;
     let productCode;
@@ -144,6 +137,7 @@ exports.SQSHandler = async (event) => {
       logger.info(`offerId: ${offerId}`);
       const mpCatClient = new MarketplaceCatalogClient();
 
+      // call DescribeEntity with offerId to get the productId
       let responseOfferId;
       try {
         responseOfferId = await mpCatClient.send(new DescribeEntityCommand({
@@ -158,6 +152,7 @@ exports.SQSHandler = async (event) => {
       productId = responseOfferId['DetailsDocument']['ProductId'];
       logger.info(`productId: ${productId}`);
 
+      // call DescribeEntity with productId to get productCode
       let responseProductId;
       try {
         responseProductId = await mpCatClient.send(new DescribeEntityCommand({
@@ -172,44 +167,24 @@ exports.SQSHandler = async (event) => {
       productCode = responseProductId['DetailsDocument']['Description']['ProductCode'];
       logger.info(`productCode: ${productCode}`);
       logger.info(`all product information together: offerId: ${offerId} productId: ${productId} productCode: ${productCode}`);
-      //const productCode = details.ProductCode;
     }
 
     //let dynamoDbKey = message['customer-identifier']
-    let dynamoDbKey = acceptorAccountId
-
-    //if (!message['customer-aws-account-id']) {
-    if (!acceptorAccountId) {
-      logger.error('customer-aws-account-id not found in message. Trying GetEntitlements');
-
-      const entitlementsResponse = await getEntitlements(
-        //message['product-code'],
-        productCode,
-        acceptorAccountId,
-        aws_region
-      );
-      
-      if (entitlementsResponse) {
-        logger.debug(`entitlementsResponse: ${JSON.stringify(entitlementsResponse, null, 2)}`);
-        
-        if (entitlementsResponse.Entitlements[0]?.CustomerAWSAccountId) {
-          logger.debug('CustomerAWSAccountId found in entitlementsResponse, will use it instead of customer-identifier (DEPRECATION March 2026) instead');
-          dynamoDbKey = entitlementsResponse.Entitlements[0].CustomerAWSAccountId;
-        }
-      }
-    }
+    let dynamoDbKey = `${acceptorAccountId}-${agreementId}`
 
     const dynamoDbParams = {
       TableName: newSubscribersTableName,
       Key: {
         customerIdentifier: { S: dynamoDbKey },
       },
-      UpdateExpression: 'set subscription_action = :ac, successfully_subscribed = :ss, subscription_expired = :se, is_free_trial_term_present = :ft',
+      UpdateExpression: 'set subscription_action = :ac, product_id = :pi, successfully_subscribed = :ss, subscription_expired = :se, is_free_trial_term_present = :ft, updated_at = :ua',
       ExpressionAttributeValues: {
         ':ac': { S: message['detail-type'] },
+        ':pi': { S: productId },
         ':ss': { BOOL: successfullySubscribed },
         ':se': { BOOL: subscriptionExpired },
-        ':ft': { BOOL: isFreeTrialTermPresent}
+        ':ft': { BOOL: isFreeTrialTermPresent},
+        ':ua': { S: new Date().toISOString() },
       },
       ReturnValues: 'UPDATED_NEW',
     };
